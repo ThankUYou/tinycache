e97df75751538503308f467947f8ebcefe0f633a
ThankUYou <2023282210229@whu.edu.cn>
2024-07-13 13:43:03 +0800

    增加注释部分

--- hash/consistenthash.go
+++ hash/consistenthash.go
@@ -6,7 +6,8 @@ import (
 	"strconv"
 )
 
-type Hash func(data []byte) uint32 //便于选择不同的哈希算法
+// Hash 定义了函数类型，采取依赖注入的方式，允许用于替换成自定义的 Hash 函数，也方便测试时替换，默认为 crc32.ChecksumIEEE 算法。
+type Hash func(data []byte) uint32
 
 type Map struct {
 	hash     Hash           //一致性哈希的哈希算法
@@ -28,7 +29,11 @@ func NewConsistentHash(replicas int, hash Hash) *Map {
 	return m
 }
 
-// Add 添加缓存节点
+// Add 添加缓存节点，允许传入 0 或 多个真实节点的名称。
+// 对每一个真实节点 key，对应创建 m.replicas 个虚拟节点，虚拟节点的名称是：strconv.Itoa(i) + key，即通过添加编号的方式区分不同虚拟节点。
+// 使用 m.hash() 计算虚拟节点的哈希值，使用 append(m.keys, hash) 添加到环上。
+// 在 hashMap 中增加虚拟节点和真实节点的映射关系。
+// 最后一步，环上的哈希值排序。
 func (m *Map) Add(keys ...string) { // 真实节点的地址
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ { // 对于每一个真实节点的地址，创建对应的虚拟节点的名字， strconv.Itoa(i) + key
@@ -40,7 +45,11 @@ func (m *Map) Add(keys ...string) { // 真实节点的地址
 	sort.Ints(m.keys)
 }
 
-// Get 获取缓存节点
+// Get 获取缓存节点，通过key获取真实节点
+// 第一步，计算 key 的哈希值。
+// 第二步，顺时针找到第一个匹配的虚拟节点的下标 idx，从 m.keys 中获取到对应的哈希值。
+// 如果 idx == len(m.keys)，说明应选择 m.keys[0]，因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
+// 第三步，通过 hashMap 映射得到真实的节点。
 func (m *Map) Get(key string) string {
 	if len(key) == 0 {
 		return ""
@@ -52,7 +61,7 @@ func (m *Map) Get(key string) string {
 		return m.keys[i] >= hash
 	}) // 在哈希环上顺时针找到第一个大于等于这个哈希值的虚拟节点的下标
 
-	return m.hashmap[m.keys[idx%len(m.keys)]] // 找到真是节点映射
+	return m.hashmap[m.keys[idx%len(m.keys)]] // 找到真实节点映射
 }
 
 // Remove 移除缓存节点
