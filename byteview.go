eb4da8536351c75db966b34ce1551b894e67b9e5
ThankUYou <2023282210229@whu.edu.cn>
2024-07-13 13:46:22 +0800

    增加注释部分

--- byteview.go
+++ byteview.go
@@ -6,22 +6,24 @@ package tinycache
 // 3. b 是只读的，使用 ByteSlice() 方法返回一个拷贝，防止缓存值被外部程序修改。
 
 type ByteView struct {
-	b []byte
+	b []byte //b 将会存储真实的缓存值。选择 byte 类型是为了能够支持任意的数据类型的存储，例如字符串、图片等。
 }
 
 func (v ByteView) Len() int {
 	return len(v.b)
-}
+} //实现 Len() int 方法，我们在 Cache 的实现中，要求被缓存对象必须实现 Value 接口，即 Len() int 方法，返回其所占的内存大小。
 
+// ByteSlice b 是只读的，使用 ByteSlice() 方法返回一个拷贝，防止缓存值被外部程序修改。
 func (v ByteView) ByteSlice() []byte {
 	return cloneBytes(v.b)
 }
 
+// String 返回string类型的缓存值
 func (v ByteView) String() string {
 	return string(v.b)
 }
 
-// 深拷贝一份返回副本
+// cloneBytes 用于创建并返回一个输入字节切片（[]byte）的副本。
 func cloneBytes(b []byte) []byte {
 	c := make([]byte, len(b))
 	copy(c, b)
